7924e5ec01cc8d98f34028c9f0be2c297077b645
jedi <350049946@qq.com>
2016-03-31 15:49:33 +0800

    0.1

--- y2wIMWeb/app.js
+++ y2wIMWeb/app.js
@@ -3,7 +3,6 @@ var http = require("http"),
 	fs = require("fs");
 http.createServer(function(req,res){
 	var pathname = url.parse(req.url).pathname;
-	console.log(pathname);
  	fs.readFile("."+pathname, function readData(err, data) {
         res.writeHead(200);
         res.end(data);
