05f8a4571c88b7df53cf901c5dc08ae4b028d87f
jedi <350049946@qq.com>
2016-03-31 17:42:43 +0800

    0.1

--- y2wIMWeb/web/js/core/config.js
+++ y2wIMWeb/web/js/core/config.js
@@ -3,5 +3,5 @@ function config(){
 }
 
 config.baseUrl = 'http://112.74.210.208:8080/v1/';
-//config.baseUrl = 'http://192.168.0.165:8080/v1/';
-config.y2wAutorizeUrl = 'http://112.74.210.208/';
+//config.baseUrl = 'http://192.168.0.104:8080/v1/';
+config.y2wAutorizeUrl = 'http://console.yun2win.com/';
