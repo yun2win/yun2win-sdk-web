eb0e31fb6b34ba8cc7e1b603544360271b109d1f
贺杰 <hejieisme@qq.com>
2016-08-23 17:21:52 +0800

    Update config.js

--- y2wIMWeb/web/js/core/config.js
+++ y2wIMWeb/web/js/core/config.js
@@ -1,6 +1,6 @@
 function config(){
 }
 
-//config.baseUrl = 'http://112.74.210.208:8080/v1/';
-config.baseUrl = 'http://localhost:18080/v1/'; 
+config.baseUrl = 'http://112.74.210.208:8080/v1/';
+//config.baseUrl = 'http://localhost:18080/v1/'; 
 config.y2wAutorizeUrl = 'http://console.yun2win.com/';
